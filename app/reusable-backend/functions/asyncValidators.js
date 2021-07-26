f14af50b24346444d2dc2c86ae72a757d87bcdf7
Michael Calizzi <calizzim@gmail.com>
2021-07-26 17:32:14 -0500

    added back reusable as a submoudle

--- app/reusable-backend/functions/asyncValidators.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  unique: async (formName, name, value, token) => {
-    let query = {}; query[name] = value
-    let result = await this.models[formName].findOne(query)
-    if (result) return { unique: 'has already been taken' }
-    return null
-  }
-}
--- app/reusable-backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const authenticator = require('../middleware/authenticator')
-
-route.get('/',authenticator,async (req,res) => {
-    return res.status(200).send({data: true})
-})
-
-module.exports = route

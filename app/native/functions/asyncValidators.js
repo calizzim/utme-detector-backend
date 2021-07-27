6a38767c977c696329ea0ab6e8721d6df28938ce
Michael Calizzi <calizzim@gmail.com>
2021-07-27 14:54:31 -0500

    deleted files to complete the template

--- app/native/functions/asyncValidators.js
+++ app/native/functions/asyncValidators.js
@@ -1,16 +1,3 @@
-const PropertyTaxes = require('../classes/PropertyTaxes')
-const propertyTaxes = new PropertyTaxes()
-
 module.exports = {
-  zipcodeOrCounty: async (database, formName, name, value, token) => {
-    let state = (await database.models.salaryInfo.findById(token.id)).state
-    let taxes = propertyTaxes.getTaxrateZipcode(value)
-    if(taxes) return null
-    taxes = propertyTaxes.getTaxrateCounty(state,value)
-    if(taxes) return null
-    let message
-    if(/\d/.test(value.slice(0,1))) message = 'this zipcode is invalid'
-    else message = state + ' does not have a county with this name'
-    return { zipcodeOrCounty: message }
-  }
+  
 }
